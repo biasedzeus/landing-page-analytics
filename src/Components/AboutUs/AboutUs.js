a0a33190171c5919adfbf1e302c42f3f8eefd60e
biasedzeus <bhanu.singh1078@gmail.com>
2021-12-07 14:08:28 +0530

    About Us Comp Added

---
+++ src/Components/AboutUs/AboutUs.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import './AboutUs.css';
+
+const AboutUs = () => {
+    return (
+      <div id="about-us" className="about-us-container">
+        <section>
+          <h1>
+            Welcome to <b>Aarna Analytics</b>
+          </h1>
+          <p>
+            We're a talented group of engineers coming from various industries
+            and academia. We all have advanced degrees and numerous years of
+            experience in the field of <b>Artificial Intelligence</b>. 
+          </p>
+          <p>
+          Our work has
+            been published at numerous conferences and journals around the
+            globe.
+            What unites us and acted as the catalyst to form Aarna Analytics is
+            our passion for this subject and our desire to use our expertise to
+            create something that will help people make better decisions. We
+            will strive to bring solutions to customers in various industries to
+            help them make tactical and strategic decisions <span>To overcome the
+            challenges they face</span>.
+          </p>
+          <p>
+            At Aarna Analytics, we believe that our technological edge and
+            expertise can lead our customers towards a successful future. We
+            will work to keep our technology ahead of the curve and accessible
+            to everyone.
+          </p>
+        </section>
+      </div>
+    );
+}
+
+export default AboutUs
