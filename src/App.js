0a6962a20744ed141c89f08e08e5e3d511566afd
biasedzeus <bhanu.singh1078@gmail.com>
2021-12-07 11:17:40 +0530

    navbar added

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import NavBar from './Components/NavBar';
+import 'bootstrap/dist/css/bootstrap.css';
+
+function App() {
+  return (
+    <div className="Apsp">
+      <NavBar/>
+    </div>
+  );
+}
+
+export default App;
