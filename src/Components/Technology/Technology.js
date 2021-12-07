5f58c1649c9726cbece1b6bcc98034b7516390e7
biasedzeus <bhanu.singh1078@gmail.com>
2021-12-07 15:33:06 +0530

    tech comp added

---
+++ src/Components/Technology/Technology.js
@@ -0,0 +1,31 @@
+import React from "react";
+import "./Technology.css";
+
+const Technology = () => {
+  return (
+    <div className="techno-container">
+      <section id="technology">
+        <h1>
+          <b>Technology</b>
+        </h1>
+        <h3>
+          Profits Prediction <b>Engine</b>
+        </h3>
+
+        <p>
+          At <span>Aarna Analytics</span> , we have undertaken the greatest challenge there
+          is. Predicting the future is a tough task, even with today's
+          technology. The Profitis Prediction Engine will use state of the art
+          methodologies to hyper-tune our models using AI to give the best
+          insights possible for a given problem. Our engine will analyze vast
+          amounts of data and output usable and measurable insights that will
+          enable our customers to reach new heights. 
+        </p>
+        <p><span>Profits</span> will be a game
+          changer!</p>
+      </section>
+    </div>
+  );
+};
+
+export default Technology;
