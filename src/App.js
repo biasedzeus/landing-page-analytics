30328e62c06841e71dc33b94edd93e459b99d3bb
biasedzeus <bhanu.singh1078@gmail.com>
2021-12-07 13:37:53 +0530

    hero comp added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import NavBar from './Components/NavBar';
 import 'bootstrap/dist/css/bootstrap.css';
+import HeroPage from './Components/HeroPage/HeroPage';
 
 function App() {
   return (
     <div className="Apsp">
       <NavBar/>
+      <HeroPage/>
     </div>
   );
 }
